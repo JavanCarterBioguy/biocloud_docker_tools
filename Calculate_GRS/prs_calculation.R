245b2b6da4633c5fb91968cbf16d955c2d1d5e09
JavanCarterBioguy <100217460+JavanCarterBioguy@users.noreply.github.com>
2023-02-27 13:08:23 -0500

    Update prs_calculation.R
    Added comments and blanked the input files

--- Calculate_GRS/prs_calculation.R
+++ Calculate_GRS/prs_calculation.R
@@ -1,4 +1,4 @@
-VOL_HER = "/heroin"
+Input_Paths = "/AWS/directories/we/need/to/pull/from"
 `%notin%` <- Negate(`%in%`)
 
 # Function for calculating PRS elements
@@ -9,13 +9,13 @@ calculatePRSElement = function(dosages, effectSizes) {
 
 # Load dosages.Dosage is a weighted metric for whatever allele is designated the alternate allele
 dosages = read.table(
-    paste0(VOL_HER, "/rti-heroin/scratch/prs/neo_abs_syn/results/treated/0005/eur/prs_dosages_outer_test.tsv"),
+    paste0(Input_Paths, "/prs_dosages_outer_test.tsv"),
     header = T
 )
 
-# Load effect sizes
+# Load effect sizes. Effect size should be provided by the paper/ papers in question.
 effects = read.table(
-    paste0(VOL_HER, "/rti-heroin/scratch/prs/neo_abs_syn/results/treated/0005/eur/prs_effect_sizes_outer_test.tsv"),
+    paste0(Input_Paths, "/prs_effect_sizes_outer_test.tsv"),
     header = T
 )
 
@@ -40,17 +40,23 @@ merged = merged[!(merged$ID %in% remove),]
 
 # Calculate PRS elements
 merged[merged['ALT_EFFECT_SIZE'] < 0, c(7:ncol(merged))] = 2 - merged[merged['ALT_EFFECT_SIZE'] < 0, c(7:ncol(merged))]
+
 merged['ALT_EFFECT_SIZE'] = abs(merged['ALT_EFFECT_SIZE'])
+
 prsElements = apply(merged[7:ncol(merged)], 2, calculatePRSElement, effectSizes = merged$ALT_EFFECT_SIZE)
+
 rownames(prsElements) = merged$ID
+
 prsElements = as.data.frame(t(prsElements))
 
 
+#Calculates the final PRS.That is done by summing the (effect size x dosage) for all SNPs included in the PRS for each individual and then dividing by the total number of SNPs (i.e., you find the average effect size x dosage across all SNPs included in the PRS for each individual). This average is the PRS for the individual.
 prs = data.frame(
   ID = rownames(prsElements),
   PRS = rep(0, nrow(prsElements))
 )
 prs["PRS"] = apply(prsElements[colnames(prsElements) %in% variants$ID], 1, sum) / nrow(variants)
+
 
 
 
